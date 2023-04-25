b5df7cac5aaa8d70500027bd34e027698183ea32
Estarlyn <estarlin212017@hotmail.com>
2023-04-25 09:57:36 +0200

    fix problem in local task desing and fix problem with url

--- resources/js/local.js
+++ resources/js/local.js
@@ -10,6 +10,9 @@ function main() {
 function task() {
     let checked = document.querySelectorAll(".checked");
     let deleteTask = document.querySelectorAll(".delete");
+    let shadow = document.querySelector('.shadow');
+    let content = document.querySelector('#content');
+    let create = document.querySelector(".create");
     let taskCreate = document.querySelector(".taskCreate");
 
     taskCreate.onsubmit = (e) => {
@@ -21,6 +24,10 @@ function task() {
                 details: document.querySelector("#details").value,
             };
             saveTask(task);
+            
+            create.classList.add("dnone");
+            shadow.classList.add('dnone');
+            content.classList.remove('blur');
         }
     };
 
@@ -41,15 +48,30 @@ function shadowMenu() {
     let create = document.querySelector(".create");
     let closeTabs = document.querySelectorAll(".closeTab");
     let createTab = document.querySelector(".createTab");
+    let content = document.querySelector('#content');
+    let openMenu = document.querySelector('#openMenu');
+    let shadow = document.querySelector('.shadow');
 
+    if(openMenu){
+        openMenu.onclick = () => {
+            content.classList.add('blur');
+            create.classList.add("dnone");
+            shadow.classList.remove('dnone');
+        };
+    }
+        
     closeTabs.forEach((closeTab) => {
         closeTab.onclick = () => {
             create.classList.add("dnone");
+            shadow.classList.add('dnone');
+            content.classList.remove('blur');
         };
     });
 
     createTab.onclick = () => {
         create.classList.remove("dnone");
+            shadow.classList.remove('dnone');
+            content.classList.add('blur');
     };
 }
 
@@ -67,7 +89,9 @@ function createTask(task, tasks) {
     let container = document.querySelector(".tasks");
     let li = document.createElement("li");
     let taskStatus = document.createElement("img");
-    let taskDelete = document.createElement("img");
+    let taskDelete = document.createElement("div");
+    let del = document.createElement('p');
+    let div = document.createElement('div');
     let info = document.createElement("div");
     let title = document.createElement("h3");
     let details = document.createElement("p");
@@ -88,9 +112,9 @@ function createTask(task, tasks) {
         updateTask(task, tasks);
     };
 
-    taskDelete.setAttribute("src", "icons/delete.svg");
-    taskDelete.setAttribute("alt", "not found");
-    taskDelete.classList.add("task__delete", "delete");
+    del.innerText ='Delete';
+    taskDelete.classList.add("close", "delete");
+    taskDelete.appendChild(del);
 
     taskDelete.onclick = () => {
         removeTask(task, tasks);
@@ -105,6 +129,7 @@ function createTask(task, tasks) {
 
     li.appendChild(taskStatus);
     li.appendChild(info);
+    li.appendChild(div);
     li.appendChild(taskDelete);
 
     container.appendChild(li);
