ec34f7ecff67df0abb24b898222da3a27233ad83
EstarlynHernandez <123844125+EstarlynHernandez@users.noreply.github.com>
2023-04-25 10:16:54 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TGroupController;
+use App\Http\Controllers\TgroupController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
