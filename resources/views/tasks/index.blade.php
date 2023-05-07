0c9239b3bb7e0e4ed22b7d512fbed6c420be8aa2
Estarlyn <estarlin212017@hotmail.com>
2023-05-07 15:15:59 +0200

    add the desktop and mobile version

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,74 +14,90 @@
 
 @section('content')
     <main class="content" id="content">
-        @if (Auth::user())
-            <h1 class="tasks__title">Your Tasks</h1>
-        @else
-            <h1 class="tasks__title">Local Tasks</h1>
-        @endif
-        <ul class="tasks container">
-            @isset($tasks)
-                @if ($tasks->count() < 1)
-                    <li>
-                        <p class="some__text">This place is empty</p>
-                    <li>
+        <div>
+
+            <div class="task__header">
+                @if (Auth::user())
+                    <h1 class="tasks__title">Your Tasks</h1>
+                @else
+                    <h1 class="tasks__title">Local Tasks</h1>
                 @endif
-                @foreach ($tasks as $task)
-                    <li id="{{ $task->id }}" class="tasks__list @if ($task['status']) task__complete @endif">
-                        <div class="task">
-                            <div class="task__checked checked">
-                                <form action="{{ route('task.check') }}" method="post">
-                                    @csrf
-                                    @method('put')
-                                    <input type="hidden" name="id" value="{{ $task['id'] }}">
-                                </form>
-                                <svg fill="transparent" width="2rem" height="2rem">
-                                    <circle cx="50%" cy="50%" r="40%" stroke="black" stroke-width="7"
-                                        stroke-dasharray="251% 251%" />
-                                    <circle cx="50%" cy="50%" r="40%" stroke="#fff" stroke-width="3"
-                                        stroke-dasharray="251% 251%" />
-                                    @if ($task['status'])
-                                        <line x1="28%" x2="45%" y1="40%" y2="70%" stroke="black"
-                                            stroke-width="5" stroke-linecap="round" />
-                                        <line x1="45%" x2="90%" y1="70%" y2="10%" stroke="black"
-                                            stroke-width="5" stroke-linecap="round" />
+                <div class="floating__button createTab">
+                    <svg fill="transparent" width="3.5rem" height="3.5rem">
+                        <line x1="50%" x2="50%" y1="20%" y2="80%" stroke="#099" stroke-width="20"
+                            stroke-linecap="round" />
+                        <line x1="80%" x2="20%" y1="50%" y2="50%" stroke="#099" stroke-width="20"
+                            stroke-linecap="round" />
+                    </svg>
+                </div>
+            </div>
+            <ul class="tasks container">
+                @isset($tasks)
+                    @if ($tasks->count() < 1)
+                        <li>
+                            <p class="some__text">This place is empty</p>
+                        <li>
+                    @endif
+                    @foreach ($tasks as $task)
+                        <li id="{{ $task->id }}"
+                            class="tasks__list @if ($task['status']) task__complete @endif">
+                            <div class="task">
+                                <div class="task__checked checked">
+                                    <form action="{{ route('task.check') }}" method="post">
+                                        @csrf
+                                        @method('put')
+                                        <input type="hidden" name="id" value="{{ $task['id'] }}">
+                                    </form>
+                                    <svg fill="transparent" width="2rem" height="2rem">
+                                        <circle cx="50%" cy="50%" r="40%" stroke="black" stroke-width="7"
+                                            stroke-dasharray="251% 251%" />
+                                        <circle cx="50%" cy="50%" r="40%" stroke="#fff" stroke-width="3"
+                                            stroke-dasharray="251% 251%" />
+                                        @if ($task['status'])
+                                            <line x1="28%" x2="45%" y1="40%" y2="70%" stroke="black"
+                                                stroke-width="5" stroke-linecap="round" />
+                                            <line x1="45%" x2="90%" y1="70%" y2="10%" stroke="black"
+                                                stroke-width="5" stroke-linecap="round" />
+                                        @endif
+                                    </svg>
+                                </div>
+                                <div class="task__info">
+                                    <h3 class="task__title">{{ $task['name'] }}</h3>
+                                    <p class="task__text">{{ Str::limit($task['details'], 10) }}</p>
+                                </div>
+                                <div class="task__time taskExtra {{ $task['type'] }}">
+                                    @if ($task['type'] == 'count')
+                                        <h3 class="task__title" style="text-transform: capitalize">{{ $task['type'] }}</h3>
+                                        <p class="task__text"><span class="task__value value">{{ $task->value }}</span>-<span
+                                                class="task__limit value-2">{{ $task->count }}</span></p>
+                                    @elseif ($task['type'] == 'time')
+                                        <h3 class="task__title" style="text-transform: capitalize">Remains</h3>
+                                        <p class="task__text"><span
+                                                class="task__minutes value">{{ intval($task->value / 60) }}</span>m <span
+                                                class="task__seconds value-2">{{ $task->value % 60 }}</span>s</p>
                                     @endif
-                                </svg>
+                                </div>
+                                <div class="close delete delete-desktop">
+                                    <form action="{{ route('task.destroy', ['task' => $task['id']]) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                    </form>
+                                    <p>Delete</p>
+                                </div>
                             </div>
-                            <div class="task__info">
-                                <h3 class="task__title">{{ $task['name'] }}</h3>
-                                <p class="task__text">{{ Str::limit($task['details'], 10) }}</p>
+                            <div class="task__delete delete">
+                                <form action="{{ route('task.destroy', ['task' => $task['id']]) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                </form>
+                                <p>Remove</p>
                             </div>
-                            <div class="task__time taskExtra {{$task['type']}}">
-                                @if ($task['type'] == 'count')
-                                    <h3 class="task__title" style="text-transform: capitalize">{{ $task['type'] }}</h3>
-                                    <p class="task__text"><span class="task__value value">{{ $task->value }}</span>-<span class="task__limit value-2">{{ $task->count }}</span></p>
-                                @elseif ($task['type'] == 'time')
-                                    <h3 class="task__title" style="text-transform: capitalize">Remains</h3>
-                                    <p class="task__text"><span class="task__minutes value">{{  intval($task->value/60)}}</span>m <span class="task__seconds value-2">{{$task->value%60 }}</span>s</p>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="task__delete delete">
-                            <form action="{{ route('task.destroy', ['task' => $task['id']]) }}" method="post">
-                                @csrf
-                                @method('delete')
-                            </form>
-                            <p>Remove</p>
-                        </div>
-                    </li>
-                @endforeach
-            @endisset
-        </ul>
+                        </li>
+                    @endforeach
+                @endisset
+            </ul>
+        </div>
     </main>
-    <div class="floating__button createTab">
-        <svg fill="transparent" width="3.5rem" height="3.5rem">
-            <line x1="50%" x2="50%" y1="10%" y2="90%" stroke="#7f7" stroke-width="10"
-                stroke-linecap="round" />
-            <line x1="90%" x2="10%" y1="50%" y2="50%" stroke="#7f7" stroke-width="10"
-                stroke-linecap="round" />
-        </svg>
-    </div>
     <section class="create dnone">
         <div class="create__header">
             <h1 class="create__title">Add Task</h1>
